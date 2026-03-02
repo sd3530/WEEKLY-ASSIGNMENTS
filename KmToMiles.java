bddfd22934b553ea94b38c5d417723b5cb54d5c0
sd3530 <sd3530@srmist.edu.in>
2026-03-02 14:05:00 +0530

    coded to console

---
+++ KmToMiles.java
@@ -0,0 +1,11 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        
+        double kilometers = 10.8;
+        double milesPerKm = 1.6;
+        
+        double miles = kilometers * milesPerKm;
+        
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
